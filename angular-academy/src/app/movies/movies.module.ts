adf0184dbf41db6327d9646931ff63ba942c520b
unknown <giorgos.mikelis@gmail.com>
2020-09-24 20:23:42 +0300

    movies-search component  generated

--- angular-academy/src/app/movies/movies.module.ts
+++ angular-academy/src/app/movies/movies.module.ts
@@ -5,15 +5,26 @@ import { MoviesListComponent } from './movies-list/movies-list.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { MovieHighlighterDirective } from './movie-highlighter.directive';
 import { MovieItemComponent } from './movies-list/movie-item/movie-item.component';
-
-
+import { MoviesSearchComponent } from './movies-search/movies-search.component';
 
 @NgModule({
-  declarations: [MoviesComponent, MoviesListComponent, MovieDetailsComponent, MovieHighlighterDirective, MovieItemComponent],
-  imports: [
-    CommonModule
+  declarations: [
+    MoviesComponent,
+    MoviesListComponent,
+    MovieDetailsComponent,
+    MovieHighlighterDirective,
+    MovieItemComponent,
+    MoviesSearchComponent,
   ],
+  imports: [CommonModule],
   providers: [],
-  exports: [MoviesComponent, MoviesListComponent, MovieDetailsComponent, MovieHighlighterDirective, MovieItemComponent]
+  exports: [
+    MoviesComponent,
+    MoviesListComponent,
+    MovieDetailsComponent,
+    MovieHighlighterDirective,
+    MovieItemComponent,
+    MoviesSearchComponent,
+  ],
 })
-export class MoviesModule { }
+export class MoviesModule {}
