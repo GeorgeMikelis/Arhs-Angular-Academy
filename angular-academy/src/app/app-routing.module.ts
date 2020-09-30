99c48ec13934b243d4e6a9cadcf0f654971d0443
unknown <giorgos.mikelis@gmail.com>
2020-09-30 19:43:20 +0300

    routing implementation 1

--- angular-academy/src/app/app-routing.module.ts
+++ angular-academy/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MoviesListComponent } from './movies/movies-list/movies-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'all-movies', component: MoviesListComponent },
+  { path: 'featured-movies', component: MoviesListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
