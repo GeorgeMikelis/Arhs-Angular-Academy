15717991d2b721f84a88b270b597f6fc1ea3f017
unknown <giorgos.mikelis@gmail.com>
2020-10-03 20:46:06 +0300

    forms validators init

--- angular-academy/src/app/app-routing.module.ts
+++ angular-academy/src/app/app-routing.module.ts
@@ -4,8 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthUsersGuard } from './auth-users.guard';
 import { Test1Component } from './movies/test-components/test1.component';
 import { SecondComponentComponent } from './movies/test-components/second-component.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
+  {
+    path: 'signup',
+    component: SignupComponent,
+    pathMatch: 'prefix'
+  },
   {
     path: 'test1-component',
     component: Test1Component,
--- angular-academy/src/app/signup/signup.component.ts
+++ angular-academy/src/app/signup/signup.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html'
 })
 export class SignupComponent implements OnInit {
+  signupForm = new FormGroup({
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    role: new FormControl('')
+  });
 
-  constructor() { }
+  constructor(private router: Router,) {}
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    console.warn(this.signupForm.value);
+  }
 }
