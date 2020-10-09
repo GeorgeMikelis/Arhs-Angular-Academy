cde3e03bd850425b1810ba88b9d61219c9ecfdad
unknown <giorgos.mikelis@gmail.com>
2020-10-09 16:19:47 +0300

    exercise 1 complete

--- Observables/angular-academy-session-8/src/app/observables/observables-exercise1/observables-exercise1.component.ts
+++ Observables/angular-academy-session-8/src/app/observables/observables-exercise1/observables-exercise1.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { fromEvent, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-observables-exercise1',
   templateUrl: './observables-exercise1.component.html',
   styleUrls: ['./observables-exercise1.component.css']
 })
-export class ObservablesExercise1Component implements OnInit {
+export class ObservablesExercise1Component implements OnInit, AfterViewInit, OnDestroy {
+  input = '';
+  keyDownObservable: Observable<KeyboardEvent>;
+  subscription: Subscription;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.keyDownObservable = fromEvent<KeyboardEvent>(document, 'keydown');
+    this.subscription = this.keyDownObservable.subscribe(key => {
+      console.log(key);
+      if(key.code === 'Escape' ) {
+        this.input = '';
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
