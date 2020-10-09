17fb5bad2ee3328a538d077dd4156d55d7fe402b
unknown <giorgos.mikelis@gmail.com>
2020-10-09 19:02:04 +0300

    exercises completed

--- Observables/angular-academy-session-8/src/app/observables/observables-exercise2/observables-exercise2.component.ts
+++ Observables/angular-academy-session-8/src/app/observables/observables-exercise2/observables-exercise2.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Testability } from "@angular/core";
+import { FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-observables-exercise2',
-  templateUrl: './observables-exercise2.component.html',
-  styleUrls: ['./observables-exercise2.component.css']
+  selector: "app-observables-exercise2",
+  templateUrl: "./observables-exercise2.component.html",
+  styleUrls: ["./observables-exercise2.component.css"],
 })
 export class ObservablesExercise2Component implements OnInit {
+  inputWithSuggestionProposition = new FormControl("");
+  words: string[] = require("an-array-of-english-words");
+  suggestions: string[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(this.words.filter((d) => /fun/.test(d)));
+    this.inputWithSuggestionProposition.valueChanges.forEach(
+      (value: string) => {
+        this.suggestions.push(value);
+        console.log(this.suggestions);
+        if (value.length > 2) {
+          this.suggestions = [];
+          for (let i = 0; i < this.words.length; i++) {
+            if (this.words[i].substr(0, value.length).toUpperCase() == value.toUpperCase()) {
+              console.log(this.words[i])
+              this.suggestions.push(this.words[i]);
+            }
+          }
+          console.log(value);
+        }
+      }
+    );
   }
-
 }
